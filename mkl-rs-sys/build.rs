4bc9b4913c385f7a07cb58abe352554f06f1ce89
novafacing <rowanbhart@gmail.com>
2024-02-21 15:52:49 -0800

    Add sdl as default feature

--- mkl-rs-sys/build.rs
+++ mkl-rs-sys/build.rs
@@ -25,6 +25,7 @@ mkl_config!("static-ilp64-seq");
 mkl_config!("static-lp64-gomp");
 mkl_config!("static-lp64-iomp");
 mkl_config!("static-lp64-seq");
+mkl_config!("sdl");
 
 #[cfg(not(any(
     feature = "dynamic-ilp64-gomp",
@@ -39,15 +40,33 @@ mkl_config!("static-lp64-seq");
     feature = "static-lp64-gomp",
     feature = "static-lp64-iomp",
     feature = "static-lp64-seq",
+    feature = "sdl",
 )))]
 compile_error!("At least one of the following features must be enabled: dynamic-ilp64-gomp, dynamic-ilp64-iomp, dynamic-ilp64-seq, dynamic-ilp64-tbb, dynamic-lp64-gomp, dynamic-lp64-iomp, dynamic-lp64-seq, dynamic-lp64-tbb, static-ilp64-gomp, static-ilp64-iomp, static-ilp64-seq, static-ilp64-tbb, static-lp64-gomp, static-lp64-iomp, static-lp64-seq, static-lp64-tbb");
+
+#[cfg(all(
+    feature = "sdl",
+    any(
+        feature = "dynamic-ilp64-gomp",
+        feature = "dynamic-ilp64-iomp",
+        feature = "dynamic-ilp64-seq",
+        feature = "dynamic-lp64-gomp",
+        feature = "dynamic-lp64-iomp",
+        feature = "dynamic-lp64-seq",
+        feature = "static-ilp64-gomp",
+        feature = "static-ilp64-iomp",
+        feature = "static-ilp64-seq",
+        feature = "static-lp64-gomp",
+        feature = "static-lp64-iomp",
+        feature = "static-lp64-seq",
+    )
+))]
+compile_error!("Set default-features = false in Cargo.toml to use dynamic-sdl");
 
 fn main() -> Result<()> {
     let library = Config::new()
         .atleast_version(MINIMUM_VERSION)
         .probe(CONFIG)?;
-
-    println!("{library:?}");
 
     let lib_paths = library.link_files.iter()
         .filter_map(|p| p.parent().map(|p| p.to_path_buf()))
